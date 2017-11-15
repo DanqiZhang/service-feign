7eb8b50f82676b1d2c85704ac6701ab25aa4cd46
Danson <1005712195@qq.com>
2017-11-15 16:12:43 +0800

    Initial commit

---
+++ src/main/java/com/danson/servicefeign/service/impl/SchedualServiceHiHystric.java
@@ -0,0 +1,14 @@
+package com.danson.servicefeign.service.impl;
+
+import com.danson.servicefeign.service.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+
+}
